ace2a2ecda46969517a830c85a01c9b3cc30c7d4
Wainii Dev <wainii1998@gmail.com>
2023-07-16 23:40:19 +0700

    Remove bg

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
         <StarsCanvas />
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <div className='herobanner'>
           <Navbar />
           <Hero />
         </div>
