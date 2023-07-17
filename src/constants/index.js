08ab9209114e944eab8a295410353910cde3445e
Wainii Dev <wainii1998@gmail.com>
2023-07-17 23:39:00 +0700

    Carousel card responsive

--- src/constants/index.js
+++ src/constants/index.js
@@ -236,7 +236,15 @@ const projects = {
     infinite: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
   },
   cards: [
     {
