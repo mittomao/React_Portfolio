346d8fb6241362212428f2e2a0694da0d6e441e0
Wainii Dev <wainii1998@gmail.com>
2024-04-08 00:02:04 +0700

    Disble config add id into file name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./"
+  base: "./",
+  build: {
+    rollupOptions: {
+      output: {
+        // Tắt việc thêm ID vào tên tệp
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+    },
+  },
 })
